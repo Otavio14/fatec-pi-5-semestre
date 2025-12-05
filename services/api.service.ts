a7bc8df67fea875559b6ce1d6e50d5fd29e60c40
Otavio14 <otaviorovere14@gmail.com>
2025-12-05 17:25:11 -0300

    Correção no auth service e login para mobile

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
-import Swal from "sweetalert2";
-import { IApiResponse } from "../types/index.type";
 import { authService } from "./auth.service";
+import { Platform } from "react-native";
 
 export const apiService = axios.create({
   // baseURL: window.location.hostname.includes("localhost")
-  baseURL: true
-    ? "http://localhost:3000"
-    : "https://fatec-pi-4-semestre-latest.onrender.com",
+  baseURL:
+    Platform.OS === "web"
+      ? "http://localhost:3000"
+      : "https://fatec-pi-4-semestre-latest.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
@@ -26,7 +26,7 @@ apiService.interceptors.request.use(
   }
 );
 
-export const errorSwal = (value) => {
+export const errorSwal = (value: { message: string }) => {
   console.log(value?.message);
 
   // const { mensagem, icone, titulo } = response?.data || {
